11a2fd5d50599fab80208e62d06629d1b2ee7802
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-08-19 19:10:36 -0300

    correções

--- src/components/Home/saiba-mais/ReadMore.jsx
+++ src/components/Home/saiba-mais/ReadMore.jsx
@@ -5,7 +5,7 @@ const ReadMore = () => {
     return (
         <>
             <div className={styles.content}>
-                <p className={styles.paragraph}>Contruir é oque fazemos</p>
+                <p className={styles.paragraph}>Construir é oque fazemos</p>
                 <h3 className={styles.h3}>Contrucy está liderando projetos de qualidade</h3>
                 <Link to={'/'}> <button className={styles.btn}>Saiba-Mais</button> </Link>
             </div>
